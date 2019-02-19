bd2a9d71764619aefa526a58f3645d297497cb22
Leifur Sigurdsson <leifurerik@gmail.com>
2019-02-19 17:23:16 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20190219170355) do
 
   create_table "doctors", force: :cascade do |t|
     t.string   "name"
+    t.string   "department"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
